9b3300ebd00e0c0d7b2ae952313760e2fb52096e
Bryan <bryan.zamora@goitsa.me>
2020-12-26 00:32:29 -0500

    first commit

---
+++ src/main/java/com/mocasystem/application/MocaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mocasystem.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mocasystem/MocaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
