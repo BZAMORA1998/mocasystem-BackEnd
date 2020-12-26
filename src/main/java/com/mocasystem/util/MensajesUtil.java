f091ed32266810aacd704d8aaae570f9bef3e8ad
Bryan <bryan.zamora@goitsa.me>
2020-12-26 11:36:27 -0500

    Se corrige mensaje

--- src/main/java/com/mocasystem/util/MensajesUtil.java
+++ src/main/java/com/mocasystem/util/MensajesUtil.java
@@ -12,6 +12,9 @@ public class MensajesUtil {
 	private static final String LOCALE_ES="es-EC";
 	private static final String LOCALE_ING="en-US";
 
+	/*
+	 * Descripcion: Traduce el mensaje en el lenguaje correspondiente
+	 */
 	public static String getMensaje(String strKey, Locale locale) {
 		bundle = ResourceBundle.getBundle(ficheroMensajes, locale);	
 		return new String(bundle.getString(strKey)
@@ -19,14 +22,6 @@ public class MensajesUtil {
 				, StandardCharsets.UTF_8);
 	}
 
-	
-	public static String getMensaje(String strKey, Object[] arrParametros, Locale locale) {
-		bundle = ResourceBundle.getBundle(ficheroMensajes, locale);
-		return new String(bundle.getString(strKey)
-				.getBytes(StandardCharsets.ISO_8859_1)
-				, StandardCharsets.UTF_8);
-	}
-	
 	/*
 	 * Descripcion: Retorna el Locate es or en.
 	 */
